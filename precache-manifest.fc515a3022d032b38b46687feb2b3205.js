ba84bf8b39b09f360db277372047a647923ceaa3
Ojasvi Singhal <osinghal00@gmail.com>
2019-05-18 17:34:37 +0530

    Updates

---
+++ precache-manifest.fc515a3022d032b38b46687feb2b3205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afef52ae144319c3330414ed25cbc825",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "1e1a8288dc2ae265926c",
+    "url": "/TicTacToe/static/css/main.29aa15be.chunk.css"
+  },
+  {
+    "revision": "c32f76b584031dbae1f5",
+    "url": "/TicTacToe/static/js/2.23bd4b01.chunk.js"
+  },
+  {
+    "revision": "1e1a8288dc2ae265926c",
+    "url": "/TicTacToe/static/js/main.3671ae50.chunk.js"
+  },
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TicTacToe/static/media/logo.5d5d9eef.svg"
+  }
+]);
